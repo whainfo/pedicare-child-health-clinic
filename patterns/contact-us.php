816f1ec9b1ceec522883824b4fb822cb80ac13d8
Veiner <aleksandr.k@webhelpagency.com>
2025-03-10 11:35:46 +0200

    Front page, FAQ page, Policy page

--- patterns/contact-us.php
+++ patterns/contact-us.php
@@ -12,14 +12,19 @@
     <h2 class="wp-block-heading has-text-align-center" style="font-style:normal;font-weight:500;text-transform:none"><mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-accent-color-color">Contact</mark>&nbsp;Us</h2>
     <!-- /wp:heading -->
 
-    <!-- wp:columns -->
-    <div class="wp-block-columns"><!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","right":"var:preset|spacing|50","bottom":"var:preset|spacing|60","left":"var:preset|spacing|50"},"blockGap":"0"},"border":{"radius":"73px"},"color":{"background":"#e6f8f880"}}} -->
+    <!-- wp:columns {"className":"PediCare-Child-Health-Clinic-contact-us"} -->
+    <div class="wp-block-columns PediCare-Child-Health-Clinic-contact-us">
+        <!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","right":"var:preset|spacing|50","bottom":"var:preset|spacing|60","left":"var:preset|spacing|50"},"blockGap":"0"},"border":{"radius":"73px"},"color":{"background":"#e6f8f880"}}} -->
         <div class="wp-block-column has-background" style="border-radius:73px;background-color:#e6f8f880;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--50)"><!-- wp:heading {"textAlign":"center","style":{"spacing":{"margin":{"right":"0","bottom":"var:preset|spacing|50"},"padding":{"top":"0","bottom":"0","left":"0","right":"0"}}}} -->
             <h2 class="wp-block-heading has-text-align-center" style="margin-right:0;margin-bottom:var(--wp--preset--spacing--50);padding-top:0;padding-right:0;padding-bottom:0;padding-left:0">Medical</h2>
             <!-- /wp:heading -->
 
             <!-- wp:code {"className":"PediCare-Child-Health-Clinic-map PediCare-Child-Health-Clinic-gap-0 PediCare-Child-Health-Clinic-p-0"} -->
-            <pre class="wp-block-code PediCare-Child-Health-Clinic-map PediCare-Child-Health-Clinic-gap-0 PediCare-Child-Health-Clinic-p-0"><code><iframe style="width:100%;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2483.4198048504795!2d-0.08615712338005012!3d51.50551377181294!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487603503d4fd043%3A0xa32a769e04757274!2zMSBCYXR0bGUgQnJpZGdlIExuLCBMb25kb24gU0UxIDJIUCwg0JLQtdC70LjQutC-0LHRgNC40YLQsNC90LjRjw!5e0!3m2!1sru!2sua!4v1692970672029!5m2!1sru!2sua" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></code></pre>
+            <div class="PediCare-Child-Health-Clinic-map PediCare-Child-Health-Clinic-gap-0 PediCare-Child-Health-Clinic-p-0">
+                <code>
+                    <iframe style="width:100%;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2483.4198048504795!2d-0.08615712338005012!3d51.50551377181294!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487603503d4fd043%3A0xa32a769e04757274!2zMSBCYXR0bGUgQnJpZGdlIExuLCBMb25kb24gU0UxIDJIUCwg0JLQtdC70LjQutC-0LHRgNC40YLQsNC90LjRjw!5e0!3m2!1sru!2sua!4v1692970672029!5m2!1sru!2sua" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </code>
+            </div>
             <!-- /wp:code -->
 
             <!-- wp:social-links {"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|40","top":"var:preset|spacing|40"}}}} -->
@@ -31,24 +36,25 @@
             <!-- /wp:social-links -->
 
             <!-- wp:paragraph -->
-            <p>6534 Glenferrie Rd test</p>
+            <p><img class="wp-image-116" style="width: 17px;" src="<?php echo esc_url(get_template_directory_uri()); ?>/src/images/icons/location-pin.png" alt="">  6534 Glenferrie Rd test</p>
             <!-- /wp:paragraph -->
 
             <!-- wp:paragraph -->
-            <p>t: 4356 KIDS DR (54234 37)</p>
+            <p><img class="wp-image-116" style="width: 17px;" src="<?php echo esc_url(get_template_directory_uri()); ?>/src/images/icons/phone.png" alt="">  t: 4356 KIDS DR (54234 37)</p>
             <!-- /wp:paragraph -->
 
             <!-- wp:paragraph -->
-            <p>f: (03) 121232134</p>
+            <p><img class="wp-image-116" style="width: 17px;" src="<?php echo esc_url(get_template_directory_uri()); ?>/src/images/icons/phone.png" alt="">  f: (03) 121232134</p>
             <!-- /wp:paragraph -->
 
             <!-- wp:paragraph -->
-            <p>med@test.com</p>
+            <p><img class="wp-image-116" style="width: 17px;" src="<?php echo esc_url(get_template_directory_uri()); ?>/src/images/icons/mail.png" alt="">  med@test.com</p>
             <!-- /wp:paragraph -->
 
             <!-- wp:paragraph -->
-            <p>Mon-Fri 09:00-17:00 / Sat 09:00-15:00</p>
-            <!-- /wp:paragraph --></div>
+            <p><img class="wp-image-116" style="width: 17px;" src="<?php echo esc_url(get_template_directory_uri()); ?>/src/images/icons/clock.png" alt="">  Mon-Fri 09:00-17:00 / Sat 09:00-15:00</p>
+            <!-- /wp:paragraph -->
+        </div>
         <!-- /wp:column -->
 
         <!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","right":"var:preset|spacing|50","bottom":"var:preset|spacing|60","left":"var:preset|spacing|50"},"blockGap":"0"},"border":{"radius":"73px"},"color":{"background":"#faecf466"}}} -->
@@ -57,7 +63,7 @@
             <!-- /wp:heading -->
 
             <!-- wp:code {"className":"PediCare-Child-Health-Clinic-map PediCare-Child-Health-Clinic-gap-0 PediCare-Child-Health-Clinic-p-0"} -->
-            <pre class="wp-block-code PediCare-Child-Health-Clinic-map PediCare-Child-Health-Clinic-gap-0 PediCare-Child-Health-Clinic-p-0"><code><iframe style="width:100%;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2483.4198048504795!2d-0.08615712338005012!3d51.50551377181294!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487603503d4fd043%3A0xa32a769e04757274!2zMSBCYXR0bGUgQnJpZGdlIExuLCBMb25kb24gU0UxIDJIUCwg0JLQtdC70LjQutC-0LHRgNC40YLQsNC90LjRjw!5e0!3m2!1sru!2sua!4v1692970672029!5m2!1sru!2sua" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></code></pre>
+            <div class="PediCare-Child-Health-Clinic-map PediCare-Child-Health-Clinic-gap-0 PediCare-Child-Health-Clinic-p-0"><code><iframe style="width:100%;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2483.4198048504795!2d-0.08615712338005012!3d51.50551377181294!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487603503d4fd043%3A0xa32a769e04757274!2zMSBCYXR0bGUgQnJpZGdlIExuLCBMb25kb24gU0UxIDJIUCwg0JLQtdC70LjQutC-0LHRgNC40YLQsNC90LjRjw!5e0!3m2!1sru!2sua!4v1692970672029!5m2!1sru!2sua" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></code></div>
             <!-- /wp:code -->
 
             <!-- wp:social-links {"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|40","top":"var:preset|spacing|40"}}}} -->
@@ -69,24 +75,25 @@
             <!-- /wp:social-links -->
 
             <!-- wp:paragraph -->
-            <p>6534 Glenferrie Rd test</p>
+            <p><img class="wp-image-116" style="width: 17px;" src="<?php echo esc_url(get_template_directory_uri()); ?>/src/images/icons/location-pin.png" alt="">  6534 Glenferrie Rd test</p>
             <!-- /wp:paragraph -->
 
             <!-- wp:paragraph -->
-            <p>t: 4356 KIDS DR (54234 37)</p>
+            <p><img class="wp-image-116" style="width: 17px;" src="<?php echo esc_url(get_template_directory_uri()); ?>/src/images/icons/phone.png" alt="">  t: 4356 KIDS DR (54234 37)</p>
             <!-- /wp:paragraph -->
 
             <!-- wp:paragraph -->
-            <p>f: (03) 121232134</p>
+            <p><img class="wp-image-116" style="width: 17px;" src="<?php echo esc_url(get_template_directory_uri()); ?>/src/images/icons/phone.png" alt="">  f: (03) 121232134</p>
             <!-- /wp:paragraph -->
 
             <!-- wp:paragraph -->
-            <p>med@test.com</p>
+            <p><img class="wp-image-116" style="width: 17px;" src="<?php echo esc_url(get_template_directory_uri()); ?>/src/images/icons/mail.png" alt="">  med@test.com</p>
             <!-- /wp:paragraph -->
 
             <!-- wp:paragraph -->
-            <p>Mon-Fri 09:00-17:00 / Sat 09:00-15:00</p>
-            <!-- /wp:paragraph --></div>
+            <p><img class="wp-image-116" style="width: 17px;" src="<?php echo esc_url(get_template_directory_uri()); ?>/src/images/icons/clock.png" alt="">  Mon-Fri 09:00-17:00 / Sat 09:00-15:00</p>
+            <!-- /wp:paragraph -->
+        </div>
         <!-- /wp:column -->
 
         <!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","right":"var:preset|spacing|50","bottom":"var:preset|spacing|60","left":"var:preset|spacing|50"},"blockGap":"0"},"border":{"radius":"73px"},"color":{"background":"#fffae5b3"}}} -->
@@ -95,7 +102,7 @@
             <!-- /wp:heading -->
 
             <!-- wp:code {"className":"PediCare-Child-Health-Clinic-map PediCare-Child-Health-Clinic-gap-0 PediCare-Child-Health-Clinic-p-0"} -->
-            <pre class="wp-block-code PediCare-Child-Health-Clinic-map PediCare-Child-Health-Clinic-gap-0 PediCare-Child-Health-Clinic-p-0"><code><iframe style="width:100%;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2483.4198048504795!2d-0.08615712338005012!3d51.50551377181294!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487603503d4fd043%3A0xa32a769e04757274!2zMSBCYXR0bGUgQnJpZGdlIExuLCBMb25kb24gU0UxIDJIUCwg0JLQtdC70LjQutC-0LHRgNC40YLQsNC90LjRjw!5e0!3m2!1sru!2sua!4v1692970672029!5m2!1sru!2sua" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></code></pre>
+            <div class="PediCare-Child-Health-Clinic-map PediCare-Child-Health-Clinic-gap-0 PediCare-Child-Health-Clinic-p-0"><code><iframe style="width:100%;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2483.4198048504795!2d-0.08615712338005012!3d51.50551377181294!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487603503d4fd043%3A0xa32a769e04757274!2zMSBCYXR0bGUgQnJpZGdlIExuLCBMb25kb24gU0UxIDJIUCwg0JLQtdC70LjQutC-0LHRgNC40YLQsNC90LjRjw!5e0!3m2!1sru!2sua!4v1692970672029!5m2!1sru!2sua" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></code></div>
             <!-- /wp:code -->
 
             <!-- wp:social-links {"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|40","top":"var:preset|spacing|40"}}}} -->
@@ -107,23 +114,23 @@
             <!-- /wp:social-links -->
 
             <!-- wp:paragraph -->
-            <p>6534 Glenferrie Rd test</p>
+            <p><img class="wp-image-116" style="width: 17px;" src="<?php echo esc_url(get_template_directory_uri()); ?>/src/images/icons/location-pin.png" alt="">  6534 Glenferrie Rd test</p>
             <!-- /wp:paragraph -->
 
             <!-- wp:paragraph -->
-            <p>t: 4356 KIDS DR (54234 37)</p>
+            <p><img class="wp-image-116" style="width: 17px;" src="<?php echo esc_url(get_template_directory_uri()); ?>/src/images/icons/phone.png" alt="">  t: 4356 KIDS DR (54234 37)</p>
             <!-- /wp:paragraph -->
 
             <!-- wp:paragraph -->
-            <p>f: (03) 121232134</p>
+            <p><img class="wp-image-116" style="width: 17px;" src="<?php echo esc_url(get_template_directory_uri()); ?>/src/images/icons/phone.png" alt="">  f: (03) 121232134</p>
             <!-- /wp:paragraph -->
 
             <!-- wp:paragraph -->
-            <p>med@test.com</p>
+            <p><img class="wp-image-116" style="width: 17px;" src="<?php echo esc_url(get_template_directory_uri()); ?>/src/images/icons/mail.png" alt="">  med@test.com</p>
             <!-- /wp:paragraph -->
 
             <!-- wp:paragraph -->
-            <p>Mon-Fri 09:00-17:00 / Sat 09:00-15:00</p>
+            <p><img class="wp-image-116" style="width: 17px;" src="<?php echo esc_url(get_template_directory_uri()); ?>/src/images/icons/clock.png" alt="">  Mon-Fri 09:00-17:00 / Sat 09:00-15:00</p>
             <!-- /wp:paragraph --></div>
         <!-- /wp:column --></div>
     <!-- /wp:columns --></div>
